e4467efabb9fe2ec29015c984637a76d2f66b694
vasunaik1999 <vasunaik1999@gmail.com>
2023-09-05 23:52:36 +0530

    first commit

---
+++ src/stores/userStore.js
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia'
+import usersData from '../assets/usersData.json' // Update the path to your JSON data file
+
+export const useUserStore = defineStore({
+  id: 'userStore',
+  state: () => ({
+    users: usersData,
+    currentUser: null // Initialize currentUser as null
+  }),
+  actions: {
+    setUser (user) {
+      this.currentUser = user
+    }
+
+  }
+})
